3a961a3616b8881bc517c31523fd3a6fa5a3aa01
martincrb <marti.cristobal@est.fib.upc.edu>
2016-03-12 09:50:26 +0100

    now the link for bluemix is generated yay

--- public/services/alchemy.js
+++ public/services/alchemy.js
@@ -1,5 +1,15 @@
 angular.module('feelinGit').factory('alchemyService', alchemyService);
 
+function generateAnalyzerURL(message) {
+  var BASE_URL = 'https://watson-api-explorer.mybluemix.net/tone-analyzer-beta/api/v3/tone?';
+  var VERSION_TAG = 'version=';
+  var TEXT_TAG = 'text=';
+  var AMP = "&";
+  var TONE_VERSION = '2016-02-11';
+  var SPACE = '%20';
+  message = message.replace(/ /g, SPACE);
+  return BASE_URL + VERSION_TAG + TONE_VERSION + AMP + TEXT_TAG + message;
+}
 function alchemyService($http, $window,  $q) {
 
     var SERVER_URL = 'http://gateway-a.watsonplatform.net/calls/text/TextGetTextSentiment';
@@ -7,6 +17,8 @@ function alchemyService($http, $window,  $q) {
     return {
         getSentiment: function(commitMessage) {
             var q = $q.defer();
+            var reqURL = generateAnalyzerURL(commitMessage);
+            console.log("TONE ANALYZER URL: "+ reqURL);
             $http.get(SERVER_URL, {
                 apikey: '770078878d0540dc544058a6d0685b6f3012aebc',
                 text: commitMessage,
