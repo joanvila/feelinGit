0f63d1b733f5bed08f539e5c2d6c7c1dedea9d88
joanvila <vila.joan94@gmail.com>
2016-03-12 02:11:00 +0100

    Node serving static content

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var path = require('path');
 
 var port = process.env.PORT || 8080;
 
@@ -12,8 +13,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-var commitsController = require('./app/routes/commits');
-app.use('/', commitsController);
+//var commitsController = require('./app/routes/commits');
+//app.use('/', commitsController);
+
+app.get('/', function(req, res, next) {
+  //Path to your main file
+  res.status(200).sendFile(path.join(__dirname+'/public/index.html'));
+});
+
 
 app.use(express.static('public'));
 
