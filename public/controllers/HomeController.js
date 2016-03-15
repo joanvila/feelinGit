73a3a0ec319a80b0cec6e766bc58f6c397aceba9
joanvila <vila.joan94@gmail.com>
2016-03-15 20:06:16 +0100

    Users must provide their own api key to use the app

--- public/controllers/HomeController.js
+++ public/controllers/HomeController.js
@@ -46,130 +46,135 @@ function HomeController($scope, $state, githubService, alchemyService) {
     };
 
     $scope.analyzeLink = function() {
-        $scope.filterDate = parseInt($scope.filterDate);
-        $scope.loading = true;
 
-        $scope.sentiment_counter = {
-            anger: 0,
-            disgust: 0,
-            fear: 0,
-            joy: 0,
-            sadness: 0
-        };
-        $scope.sentiments = {
-            anger: 0,
-            disgust: 0,
-            fear: 0,
-            joy: 0,
-            sadness: 0
-        };
-        $scope.commits_per_sentiment = {
-            anger: [],
-            disgust: [],
-            fear: [],
-            joy: [],
-            sadness: []
-        };
-        $scope.languages = [];
+        if (typeof($scope.inputData) === 'undefined') alert('You must fill the github url and the alchemy API key');
+        else if (typeof($scope.inputData.url) === 'undefined') alert('You must provide a github repository url');
+        else if (typeof($scope.inputData.apiKey) === 'undefined') alert('You must provide an Alchemy API Key. It is very easy to get one');
+        else {
+            $scope.filterDate = parseInt($scope.filterDate);
+            $scope.loading = true;
 
-        var link = $scope.inputData.url;
-        var elem = link.split("/");
-        var owner = elem[elem.length-2];
-        var repo = elem[elem.length-1];
+            $scope.sentiment_counter = {
+                anger: 0,
+                disgust: 0,
+                fear: 0,
+                joy: 0,
+                sadness: 0
+            };
+            $scope.sentiments = {
+                anger: 0,
+                disgust: 0,
+                fear: 0,
+                joy: 0,
+                sadness: 0
+            };
+            $scope.commits_per_sentiment = {
+                anger: [],
+                disgust: [],
+                fear: [],
+                joy: [],
+                sadness: []
+            };
+            $scope.languages = [];
 
-        $scope.valid_commits = 0;
+            var link = $scope.inputData.url;
+            var elem = link.split("/");
+            var owner = elem[elem.length-2];
+            var repo = elem[elem.length-1];
 
-        githubService.getCommits(owner, repo).then(function(commits) {
-            commits = commits.data;
-            for (var i = 0; i < commits.length; ++i) {
-                var commitDate = commits[i].commit.author.date;
-                var momentDate = moment(commitDate);
+            $scope.valid_commits = 0;
 
-                var goodCommit = true;
-                if ($scope.filterDate) goodCommit = (momentDate.add(1, 'days').valueOf() > moment().valueOf());
-                if (goodCommit) {
+            githubService.getCommits(owner, repo).then(function(commits) {
+                commits = commits.data;
+                for (var i = 0; i < commits.length; ++i) {
+                    var commitDate = commits[i].commit.author.date;
+                    var momentDate = moment(commitDate);
 
-                    alchemyService.getSentiment(commits[i].commit.message, commits[i].commit.author.name).then(function(sentiment) {
-                        var status = sentiment.data.status;
-                        var author = sentiment.data.author;
+                    var goodCommit = true;
+                    if ($scope.filterDate) goodCommit = (momentDate.add(1, 'days').valueOf() > moment().valueOf());
+                    if (goodCommit) {
 
-                        if (status != "ERROR") {
-                          var emotions = sentiment.data.docEmotions;
-                          $scope.sentiment_counter['anger'] += parseFloat(emotions['anger'])*200;
-                          $scope.sentiment_counter['joy'] += parseFloat(emotions['joy'])*200;
-                          $scope.sentiment_counter['fear'] += parseFloat(emotions['fear'])*200;
-                          $scope.sentiment_counter['sadness'] += parseFloat(emotions['sadness'])*200;
-                          $scope.sentiment_counter['disgust'] += parseFloat(emotions['disgust'])*200;
-                          $scope.valid_commits += 1;
+                        alchemyService.getSentiment(commits[i].commit.message, commits[i].commit.author.name, $scope.inputData.apiKey).then(function(sentiment) {
+                            var status = sentiment.data.status;
+                            var author = sentiment.data.author;
 
-                          if (parseFloat(emotions['anger']) >= 0.6) $scope.commits_per_sentiment['anger'].push({user: author, commit: sentiment.data.text});
-                          if (parseFloat(emotions['joy']) >= 0.6) $scope.commits_per_sentiment['joy'].push({user: author, commit: sentiment.data.text});
-                          if (parseFloat(emotions['fear']) >= 0.6) $scope.commits_per_sentiment['fear'].push({user: author, commit: sentiment.data.text});
-                          if (parseFloat(emotions['sadness']) >= 0.6) $scope.commits_per_sentiment['sadness'].push({user: author, commit: sentiment.data.text});
-                          if (parseFloat(emotions['disgust']) >= 0.6) $scope.commits_per_sentiment['disgust'].push({user: author, commit: sentiment.data.text});
+                            if (status != "ERROR") {
+                              var emotions = sentiment.data.docEmotions;
+                              $scope.sentiment_counter['anger'] += parseFloat(emotions['anger'])*200;
+                              $scope.sentiment_counter['joy'] += parseFloat(emotions['joy'])*200;
+                              $scope.sentiment_counter['fear'] += parseFloat(emotions['fear'])*200;
+                              $scope.sentiment_counter['sadness'] += parseFloat(emotions['sadness'])*200;
+                              $scope.sentiment_counter['disgust'] += parseFloat(emotions['disgust'])*200;
+                              $scope.valid_commits += 1;
 
-                          $scope.sentiments.anger = $scope.sentiment_counter['anger'] / $scope.valid_commits;
-                          $scope.sentiments.disgust = $scope.sentiment_counter['disgust'] / $scope.valid_commits;
-                          $scope.sentiments.fear = $scope.sentiment_counter['fear'] / $scope.valid_commits;
-                          $scope.sentiments.joy = $scope.sentiment_counter['joy'] / $scope.valid_commits;
-                          $scope.sentiments.sadness = $scope.sentiment_counter['sadness'] / $scope.valid_commits;
+                              if (parseFloat(emotions['anger']) >= 0.6) $scope.commits_per_sentiment['anger'].push({user: author, commit: sentiment.data.text});
+                              if (parseFloat(emotions['joy']) >= 0.6) $scope.commits_per_sentiment['joy'].push({user: author, commit: sentiment.data.text});
+                              if (parseFloat(emotions['fear']) >= 0.6) $scope.commits_per_sentiment['fear'].push({user: author, commit: sentiment.data.text});
+                              if (parseFloat(emotions['sadness']) >= 0.6) $scope.commits_per_sentiment['sadness'].push({user: author, commit: sentiment.data.text});
+                              if (parseFloat(emotions['disgust']) >= 0.6) $scope.commits_per_sentiment['disgust'].push({user: author, commit: sentiment.data.text});
 
-                          var maxFeeling = 'anger';
-                          var maxNumber = $scope.sentiment_counter['anger'];
+                              $scope.sentiments.anger = $scope.sentiment_counter['anger'] / $scope.valid_commits;
+                              $scope.sentiments.disgust = $scope.sentiment_counter['disgust'] / $scope.valid_commits;
+                              $scope.sentiments.fear = $scope.sentiment_counter['fear'] / $scope.valid_commits;
+                              $scope.sentiments.joy = $scope.sentiment_counter['joy'] / $scope.valid_commits;
+                              $scope.sentiments.sadness = $scope.sentiment_counter['sadness'] / $scope.valid_commits;
 
-                          for (var key in $scope.sentiment_counter) {
-                              if ($scope.sentiment_counter.hasOwnProperty(key)) {
-                                  if ($scope.sentiment_counter[key] > maxNumber) {
-                                      maxNumber = $scope.sentiment_counter[key];
-                                      maxFeeling = key;
+                              var maxFeeling = 'anger';
+                              var maxNumber = $scope.sentiment_counter['anger'];
+
+                              for (var key in $scope.sentiment_counter) {
+                                  if ($scope.sentiment_counter.hasOwnProperty(key)) {
+                                      if ($scope.sentiment_counter[key] > maxNumber) {
+                                          maxNumber = $scope.sentiment_counter[key];
+                                          maxFeeling = key;
+                                      }
                                   }
                               }
-                          }
 
-                          switch (maxFeeling) {
-                              case 'anger':
-                              $scope.octoName = 'luchadortocat.png';
-                              break;
-                              case 'disgust':
-                              $scope.octoName = 'steroidtocat.png';
-                              break;
-                              case 'fear':
-                              $scope.octoName = 'grim-repo.jpg';
-                              break;
-                              case 'joy':
-                              $scope.octoName = 'nyantocat.gif';
-                              break;
-                              case 'sadness':
-                              $scope.octoName = 'deckfailcat.png';
-                              break;
-                              default:
-                              break;
-                          }
+                              switch (maxFeeling) {
+                                  case 'anger':
+                                  $scope.octoName = 'luchadortocat.png';
+                                  break;
+                                  case 'disgust':
+                                  $scope.octoName = 'steroidtocat.png';
+                                  break;
+                                  case 'fear':
+                                  $scope.octoName = 'grim-repo.jpg';
+                                  break;
+                                  case 'joy':
+                                  $scope.octoName = 'nyantocat.gif';
+                                  break;
+                                  case 'sadness':
+                                  $scope.octoName = 'deckfailcat.png';
+                                  break;
+                                  default:
+                                  break;
+                              }
 
-                        }
+                            }
 
-                    });
+                        });
+                    }
                 }
-            }
-            $scope.loading = false;
-        });
+                $scope.loading = false;
+            });
 
-        githubService.getLanguages(owner, repo).then(function(languages) {
-            $scope.valid_commits = true;
-            var totalLines = 0;
-            for (key in languages.data){
-                var aux = {
-                    language: key,
-                    lines: languages.data[key]
-                };
-                $scope.languages.push(aux);
-                totalLines += languages.data[key];
-            }
+            githubService.getLanguages(owner, repo).then(function(languages) {
+                $scope.valid_commits = true;
+                var totalLines = 0;
+                for (key in languages.data){
+                    var aux = {
+                        language: key,
+                        lines: languages.data[key]
+                    };
+                    $scope.languages.push(aux);
+                    totalLines += languages.data[key];
+                }
 
-            for (var i = 0; i < $scope.languages.length; ++i) {
-                $scope.languages[i].percent = ($scope.languages[i].lines/totalLines)*100;
-            }
-        });
+                for (var i = 0; i < $scope.languages.length; ++i) {
+                    $scope.languages[i].percent = ($scope.languages[i].lines/totalLines)*100;
+                }
+            });
+        }
     }
-
 }
--- public/services/alchemy.js
+++ public/services/alchemy.js
@@ -2,8 +2,6 @@ angular.module('feelinGit').factory('alchemyService', alchemyService);
 
 function generateAnalyzerURL(message) {
   var BASE_URL = 'http://gateway-a.watsonplatform.net/calls/text/TextGetEmotion?';
-  var APIKEY_TAG = 'apikey=';
-  var API_KEY = 'd4f4da85c4ad1f47664d8124467f66a683e7e684';
   var TEXT_TAG = 'text=';
   var AMP = "&";
   var OUTPUT_TAG = 'outputMode=';
@@ -13,14 +11,14 @@ function generateAnalyzerURL(message) {
   var SOURCE = 1;
 
   message = message.replace(/ /g, SPACE);
-  return BASE_URL + APIKEY_TAG + API_KEY + AMP + OUTPUT_TAG + OUTPUT + AMP + TEXT_TAG + message + AMP + SOURCE_TAG + SOURCE;
+  return BASE_URL + OUTPUT_TAG + OUTPUT + AMP + TEXT_TAG + message + AMP + SOURCE_TAG + SOURCE;
 }
 function alchemyService($http, $window,  $q) {
 
     return {
-        getSentiment: function(commitMessage, author) {
+        getSentiment: function(commitMessage, author, apiKey) {
             var q = $q.defer();
-            var reqURL = generateAnalyzerURL(commitMessage);
+            var reqURL = generateAnalyzerURL(commitMessage) + '&apikey=' + apiKey;
             $http.get(reqURL).then(function(data) {
                 data.data.author = author;
                 q.resolve(data);
